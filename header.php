dd97a544287301af2c7e9728504f4f29a38c327d
noelmoriano <mariano260996@gmail.com>
2019-12-31 12:22:11 -0500

    config template wordpress

--- header.php
+++ header.php
@@ -4,7 +4,26 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php bloginfo('title');?></title>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+
+	<!-- ================= Open Graph ================== -->
+		<meta name="Diseño de soluciones" content="Somos un laboratorio de innovación apasionados por resolver problemas que nadie puede.">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="Ruptivo">
+        <meta name="Description" content="laboratorio creativo innovación design thinking brand web app digital experiencia de usuario">
+
+        <!-- ================= Favicons ================== -->
+        <!-- Standard -->
+        <link rel="shortcut icon" href="<?php bloginfo('template_directory');?>/images/favicon.png">
+        <!-- Retina iPad Touch Icon-->
+        <link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('template_directory');?>/images/favicon.png">
+        <!-- Retina iPhone Touch Icon-->
+        <link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('template_directory');?>/images/favicon.png">
+        <!-- Standard iPad Touch Icon--> 
+        <link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('template_directory');?>/images/favicon.png">
+        <!-- Standard iPhone Touch Icon--> 
+        <link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('template_directory');?>/images/favicon.png">
+        <!-- Font awesome--> 
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
 	
 	<?php wp_head(); ?>
 
